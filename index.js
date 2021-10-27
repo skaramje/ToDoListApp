95e11d29fcd914a0d1599e5d049f0f70c4db13a8
skaramje <skaramje@gmail.com>
2021-10-27 14:43:55 -0600

    complete app with styling

--- index.js
+++ index.js
@@ -26,13 +26,15 @@ function App(){
     }
 
     return(
-        <>
-        {todos.map((todo, i) => (
-            <Todo index={i} key={i} todo={todo} remove={removeTodo} />
-        ))}
-        
-        <TodoForm addTodo={addTodo}/>
-        </>
+        <div className="app">
+            <div className="todo-list">
+            {todos.map((todo, i) => (
+                <Todo index={i} key={i} todo={todo} remove={removeTodo} />
+            ))}
+            
+            <TodoForm addTodo={addTodo}/>
+            </div>
+        </div>
     );
 }
 
