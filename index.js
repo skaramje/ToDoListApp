8ce25a143cb5a63920f74b366d272bce5522f0e5
skaramje <skaramje@gmail.com>
2021-10-27 13:05:20 -0600

    add user input to the list

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ function App(){
             text: 'build todo app',
             isCompleted: false,
         }
-    ])
+    ]);
+    const [value, setValue] = React.useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!value) return;
+        const newTodos = [...todos,{text:value, isCompleted:false}];
+        setTodos(newTodos);
+        setValue('');
+    }
 
     return(
         <>
         {todos.map((todo, i) =>(
         <div className='todo' key={i}>{todo.text}</div>
         ))}
+        <form onSubmit={handleSubmit}>
+            <input
+                type = "text"
+                className = "input"
+                value = {value}
+                placeholder = 'add todo...'
+                onChange = {e => setValue(e.target.value)}
+            />
+        </form>
         </>
     );
 }
