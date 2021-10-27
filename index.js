95dee676a5d123e47023d0a88a12ee5b4b6b838f
skaramje <skaramje@gmail.com>
2021-10-27 12:51:11 -0600

    update styles

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function App(){
 
     return(
         <>
-        {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+        {todos.map((todo, i) =>(
+        <div className='todo' key={i}>{todo.text}</div>
+        ))}
         </>
     );
 }
